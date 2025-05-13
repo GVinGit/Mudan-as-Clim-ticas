45f8c7168c489692be1a22befba57ee304642a51
Gabriel Vinicius <gabrielviniciusdecs@gmail.com>
2025-05-13 10:06:04 -0300

    Update script.js

--- MudancasClimaticas/cadastro/script.js
+++ MudancasClimaticas/cadastro/script.js
@@ -1,4 +1,3 @@
-
 const form = document.getElementById("form");
 const lista = document.getElementById("clima");
 
@@ -6,12 +5,11 @@ let cadastro = JSON.parse(localStorage.getItem("cadastro")) || [];
 
 function renderInformacoes() {
   lista.innerHTML = "";
-
   cadastro.forEach((item, index) => {
     const li = document.createElement("li");
     li.innerHTML = `
       <strong>${item.nome}</strong><br>
-      sobrenome: ${item.sobrenome}<br>
+      Sobrenome: ${item.sobrenome}<br>
       Email: ${item.email}<br>
       Senha: ${item.senha}<br>
       <button onclick="editar(${index})">Editar</button>
@@ -25,7 +23,7 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const novaInformacao = {
-    nome: form["nome"].value,
+    nome: form["user"].value,
     sobrenome: form["sobrenome"].value,
     email: form["email"].value,
     senha: form["senha"].value,
@@ -34,6 +32,17 @@ form.addEventListener("submit", function (e) {
 
   if (novaInformacao.senha !== novaInformacao.confirmarSenha) {
     alert("As senhas não coincidem!");
+    return;
+  }
+
+  const confirmacao = confirm("Deseja prosseguir com o cadastro ou editar as informações?");
+  
+  if (!confirmacao) {
+    form["user"].value = novaInformacao.nome;
+    form["sobrenome"].value = novaInformacao.sobrenome;
+    form["email"].value = novaInformacao.email;
+    form["senha"].value = novaInformacao.senha;
+    form["confirmar-senha"].value = novaInformacao.confirmarSenha;
     return;
   }
 
@@ -51,13 +60,13 @@ function remover(index) {
 
 function editar(index) {
   const informacao = cadastro[index];
-  form["nome"].value = informacao.nome;
-  form["sobrenome"].value = informacao.sobrenome;
-  form["email"].value = informacao.email;
-  form["senha"].value = informacao.senha;
-  form["confirmar-senha"].value = informacao.senha;
-
+  form["user\n"].value = informacao.nome;
+  form["sobrenome\n"].value = informacao.sobrenome;
+  form["email\n"].value = informacao.email;
+  form["senha\n"].value = informacao.senha;
+  form["confirmar-senha\n"].value = informacao.senha;
   cadastro.splice(index, 1);
+  localStorage.setItem("cadastro", JSON.stringify(cadastro));
   renderInformacoes();
 }
 
